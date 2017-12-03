b7d54f02beb37bfc9e35407f5f31f67b1e54a0ea
Andrew Che <andrew@codeninja55.me>
2017-12-04 04:40:16 +1100

    Added images for list

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,85 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  {
+    id: 1,
+    name: 'Captain America',
+    realName: 'Steven "Steve" Rogers',
+    abilities: 'Captain America had mastered the martial arts of American-style boxing and judo, and had combined these disciplines with ' +
+    'his own unique hand-to-hand style of combat. He had also shown skill and knowledge of a number of other martial arts. He engaged in ' +
+    'a daily regimen of rigorous exercise (including aerobics, weight lifting, gymnastics, and simulated combat) to keep himself in peak ' +
+    'condition. Captain America was one of the finest human combatants Earth had ever known.'
+  },
+  {
+    id: 2,
+    name: 'Iron Man',
+    realName: 'Anthony Edward "Tony" Stark',
+    abilities: 'Tony has a genius level intellect that allows him to invent a wide range of sophisticated devices, specializing in ' +
+    'advanced weapons and armor. He possesses a keen business mind.'
+  },
+  {
+    id: 3,
+    name: 'Thor',
+    realName: 'Thor Odinson',
+    abilities: 'Thor is trained in the arts of war, being a superbly skilled warrior, highly proficient in hand-to-hand combat, ' +
+    'swordsmanship and hammer throwing.'
+  },
+  {
+    id: 4,
+    name: 'Hulk',
+    realName: 'Robert Bruce Banner',
+    abilities: 'Dr. Bruce Banner is a genius in nuclear physics, possessing a mind so brilliant that it cannot be measured on any ' +
+    'known intelligence test. When Banner is the Hulk, Banner\'s consciousness is buried within the Hulk\'s, and can influence the ' +
+    'Hulk\'s behavior only to a very limited extent.'
+  },
+  {
+    id: 5,
+    name: 'Black Widow',
+    realName: 'Natasha Romanova',
+    abilities: 'Natasha Romanova, known by many aliases, is an expert spy, athlete, and assassin. Trained at a young age by the ' +
+    'KGB\'s infamous Red Room Academy, the Black Widow was formerly an enemy to the Avengers. She later became their ally after ' +
+    'breaking out of the U.S.S.R.\'s grasp, and also serves as a top S.H.I.E.L.D. agent.'
+  },
+  {
+    id: 6,
+    name: 'Ant-Man',
+    realName: 'Scott Lang',
+    abilities: 'Expertise in electronics.'
+  },
+  {
+    id: 7,
+    name: 'Hawkeye',
+    realName: 'Clinton Francis "Clint" Barton',
+    abilities: 'Ronin is a world-class archer and marksman. His above average reflexes and hand-eye-coordination make him the most ' +
+    'proficient archer ever known. He is also trained to throw knifes, darts, balls, bolas and boomerangs. He is natural athlete. He ' +
+    'is also formidable unarmed combatant, thanks largely for longtime combat training with Captain America. He also has extensive ' +
+    'training as an acrobat and aerialist. He is highly capable and charismatic team leader and a shrewd combat strategist, albeit ' +
+    'sometimes reckless. Barton is also talented weapon designer, particularly well-versed in variations on basic traditional weaponry ' +
+    'such as arrows, blades and hand-thrown projectiles. He has designed and crafted crescent darts, boomerangs, throwing irons, bolas, ' +
+    'axes, custom arrows and bows. he is experienced motorcycle rider, Barton was one of the of the most proficient and daring pilots ' +
+    'of the Avengers\' supersonic Quinjets and other aircraft. He was once 80% deaf due to an injury, but his hearing was restored ' +
+    'during his rebirth on Franklin Richards\' Counter-Earth'
+  },
+  {
+    id: 8,
+    name: 'Doctor Strange',
+    realName: 'Stephen Vincent Strange',
+    abilities: 'Doctor Strange is a skilled athlete and martial artist with substantial medical and magical knowledge. Though an ' +
+    'expert surgeon, Strange\'s nerve-damaged hands prevent him from performing surgery except when supplemented by magic.'
+  },
+  {
+    id: 9,
+    name: 'Black Panther',
+    realName: 'T\'Challa',
+    abilities: 'T\'Challa is a brilliant tactician, strategist, scientist, tracker and a master of all forms of unarmed combat whose ' +
+    'unique hybrid fighting style incorporates acrobatics and aspects of animal mimicry. T\'Challa being a royal descendant of a warrior ' +
+    'race is also a master of armed combat, able to use a variety of weapons but prefers unarmed combat. He is a master planner who ' +
+    'always thinks several steps ahead and will go to extreme measures to achieve his goals and protect the kingdom of Wakanda.'
+  },
+  {
+    id: 10,
+    name: 'Spider-Man',
+    realName: 'Peter Benjamin Parker',
+    abilities: 'Peter is an accomplished scientist, inventor and photographer.'
+  }
+];
