fab7da2fa06a6d189a21c5664f192e55d84cf5ad
Andrew Che <andrew@codeninja55.me>
2017-12-04 12:13:27 +1100

    Added a onSelect method to show the Hero details for each list item

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-// import { Hero } from '../hero';
+import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
 
 @Component({
@@ -9,13 +9,14 @@ import { HEROES } from '../mock-heroes';
 })
 
 export class HeroesComponent implements OnInit {
-  // hero: Hero = {
-  //   id: 1,
-  //   name: 'Captain America',
-  // };
   heroes = HEROES;
+  selectedHero: Hero;
 
   constructor() { }
 
   ngOnInit() { }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
