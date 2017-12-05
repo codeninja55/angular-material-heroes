5d250c1210089c619e82901708495fca56337c7e
Andrew Che <andrew@codeninja55.me>
2017-12-05 22:19:11 +1100

    Added messages and heroes components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,21 +7,24 @@ import { MaterialModule } from './material.module';
 import { MatNavbarComponent  } from './mat-navbar/mat-navbar.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroService } from './hero.service';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MatNavbarComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
     MaterialModule,
     FormsModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ HeroService ],
+  bootstrap: [ AppComponent ]
 })
 
 export class AppModule { }
