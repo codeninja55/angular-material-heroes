a98be05ed892c1f0615adf00669a4db06c1cc204
Andrew Che <andrew@codeninja55.me>
2017-12-09 03:00:22 +1100

    Completed changing existing methods to use HttpClient instead of RxJS

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,26 +1,59 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {catchError, tap} from 'rxjs/operators';
 
 @Injectable()
 export class HeroService {
+  private heroesUrl = 'api/heroes';  // URL to web api
 
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
 
-  getHeroes(): Observable<Hero[]> {
-    // TODO: send the message _after_ fetching the heroes
-    // this.messageService.add('HeroService: fetched heroes');
-    this.messageService.showSnackBar('HeroService: fetched heroes');
-
-    return of(HEROES);
+  /** Log a HeroService message with the Angular Material Snackbar */
+  private log(message: string) {
+    this.messageService.showSnackBar('HeroService: ' + message);
   }
 
+  // New HttpClient method
+  getHeroes(): Observable<Hero[]> {
+    // this.messageService.add('HeroService: fetched heroes');
+    return this.http.get<Hero[]>(this.heroesUrl).pipe(
+      tap(() => this.log('fetched heroes.')),
+      catchError(this.handleError('getHeroes', []))
+    );
+  }
+
+  // New HttpClient method
   getHero(id: number): Observable<Hero> {
-    // TODO: send the message _after_ fetching the hero
-    this.messageService.showSnackBar(`HeroService: fetched hero id=${id}`);
-    return of(HEROES.find(hero => hero.id === id));
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 }
