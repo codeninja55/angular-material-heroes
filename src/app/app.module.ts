9ce6e46c8e0da3e79ffc8ff445158901907c416a
Andrew Che <andrew@codeninja55.me>
2017-12-11 13:56:32 +1100

    Added comments for imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 
 // All Angular Material Modules imported at once
 import { MaterialModule } from './material.module';
 
+// Custom Project Components
 import { AppComponent } from './app.component';
 import { MatNavbarComponent  } from './mat-navbar/mat-navbar.component';
 import { HeroesComponent } from './heroes/heroes.component';
@@ -13,8 +16,6 @@ import { HeroService } from './hero.service';
 import { MessageService } from './message.service';
 import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { HttpClientModule } from '@angular/common/http';
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService } from './in-memory-data.service';
 import { HeroFormComponent } from './hero-form/hero-form.component';
 
